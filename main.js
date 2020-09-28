1cb73d0e89a9dcbd64ea46f0b45ca478e49d8a41
joshua londono <joshualondono@outlook.com>
2020-09-28 10:53:08 -0400

    updated

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-// Use any ONE of these API's that needs an API key to get data.
-// Read the docs to see how to call for and get the data that you want
-// Or you can use one of your choosing if you are comfortable, HOWEVER it must require an API Key
-// Parse the data, and log any important information you would like to show in a nice format of your choosing.
-// You can add your own spin to the data in how you present it.
-// Use any tools we've practiced such as promises, destructuring, etc.
-// Once everything works, upload the file and submit.
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('a63f62608ebf43d4b855f19cde053677');
+const readlineSync = require('readline-sync');
 
 
-// // Weather API- https://openweathermap.org/api
-// // Weather/Air Quality API-  https://www.airvisual.com/dashboard/api
-// // Recipe API- https://developer.edamam.com/ (uses two key code)
-// // Superheroes API- https://superheroapi.com/index.html
-// // Movies API- https://www.omdbapi.com/
-// // Sports API - https://allsportsapi.com/
-// // News API - https://newsapi.org/
-// // Harvard Art Museum API - https://www.harvardartmuseums.org/collections/api
+//SEARCH BUCKETS
+  const bias = ['Left','Right', 'Moderate']
+  const left = ['cnn.com', 'bbc.co.uk'];
+  const right = ['foxnews.com'];
 
+//OBJECT QUERY
+  const makeQuery = (source) => {
 
-// If you want to make your project a little more robust and dynamic you could use tools like the ones below:
-// Read the docs to see how it works. Not hard.
+    const objQuery = {}
 
+  if (source === 'Left'){
+    const newQuery = Object.create(objQuery);
+    newQuery.q = 'election';
+    newQuery.sources = [...left].toString();
+    newQuery.sortBy = 'relevancy';
+    console.log(newQuery);
+  } else {
+    const newQuery = Object.create(objQuery);
+    newQuery.q = 'election';
+    newQuery.sources = [...right].toString();
+    newQuery.sortBy = 'relevancy';
+    console.log(newQuery);
+  }
+};
 
-// Readline: https://nodejs.org/api/readline.html
-// OR
-// Inquirer: https://www.npmjs.com/package/inquirer
-// Read the docs to find out how to use. Pretty intuitive.
+  //USER INPUT
+
+  // const userInput = ()
+    const index = readlineSync.keyInSelect(bias, 'Set Your Bias Index?');
+    const query = makeQuery(bias[index]);
+
+    const apiCall = (search) => {
+    newsapi.v2.everything(search).then(response => {
+      console.log(response);
+    });
+  };
+
+    console.log(apiCall(query));
+
+    //apiCall(query);
+
+  //API CALL
