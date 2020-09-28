03fafd0bfa304d0c18dce68861917882c7d32e43
joshua londono <joshualondono@outlook.com>
2020-09-28 13:51:26 -0400

    update

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ const readlineSync = require('readline-sync');
 
 
 //SEARCH BUCKETS
-  const bias = ['Left','Right', 'Moderate']
-  const left = ['cnn.com', 'bbc.co.uk'];
-  const right = ['foxnews.com'];
+const bias = ['Left','Right', 'Moderate']
+const left = ['cnn.com', 'bbc.co.uk'];
+const right = ['foxnews.com', 'thefederalist.com'];
+const moderate = ['apnews.com', 'npr.org']
 
 //OBJECT QUERY
-  const makeQuery = (term, source) => {
+const makeQuery = (term, source) => {
 
-    const objQuery = {}
+
+  const objQuery = {}
 
   if (source === 'Left'){
     // const newQuery = Object.create(objQuery);
@@ -24,11 +26,16 @@ const readlineSync = require('readline-sync');
   language: 'en',
   sortBy: 'relevancy',
 }).then(response => {
-      console.log(response);
-    });
-    return newQuery
 
-  } else {
+  console.log(`
+         HEADLINES FROM THE LEFT`)
+      console.log(response.articles.filter( ({author, title, description, url}) => console.log(`
+         Headline: ${title}
+         Author:   ${author}
+         Source:   ${url}`)));
+    });
+
+  } else if (source === 'Right'){
     // const newQuery = Object.create(objQuery);
     // newQuery.q = 'election';
     // newQuery.sources = [...right].toString();
@@ -39,14 +46,47 @@ const readlineSync = require('readline-sync');
   language: 'en',
   sortBy: 'relevancy',
 }).then(response => {
-      console.log(response);
+
+      console.log(`
+         HEADLINES FROM THE RIGHT`)
+      console.log(response.articles.filter( ({author, title, description, url}) => console.log(`
+         Headline: ${title}
+         Author:   ${author}
+         Source:   ${url}`)));
     });
-    return newQuery
-   }
+
+  } else {
+    newsapi.v2.everything({
+  q: term,
+  domains: [...moderate].toString(),
+  language: 'en',
+  sortBy: 'relevancy',
+}).then(response => {
+  console.log(`
+         MODERATE HEADLINES`)
+      console.log(response.articles.filter( ({author, title, description, url}) => console.log(`
+         Headline: ${title}
+         Summary:  ${description}
+         Source:   ${url}`)));
+    });
+  }
 };
 
   //USER INPUT
-    const index = readlineSync.keyInSelect(bias, 'Set Your Bias Index?');
-    const query = makeQuery('election',bias[index]);
+    const index = readlineSync.keyInSelect(bias, 'Set your bias index');
+    const searchTerm = readlineSync.question('Enter your search term: ');
+    const query = makeQuery(searchTerm,bias[index]);
+
+  //   const formattedQuery = query.articles.filter(({author, title, description, url}) => {
+  //
+  //     console.log(
+  //
+  //       `Headline: ${title},
+  //   Author: ${author},
+  //   Summary: ${desription},
+  //   Source: ${url}
+  //   -------------`)
+  // });
+
 
 console.log(query)
